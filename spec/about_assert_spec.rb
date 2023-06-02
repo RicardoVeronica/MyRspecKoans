beaf1a013a7859ab5e86195d2522115d28ab62fe
ricardoveronica <ricardo_veronica.duran@hotmail.com>
2023-06-02 11:37:54 -0600

    adds about_assert

--- spec/about_assert_spec.rb
+++ spec/about_assert_spec.rb
@@ -1,3 +1,19 @@
 describe 'about assert' do
-  it 'show a skipped message'
+  it 'if you want to pass a test, everything should be match' do
+    expect(true).to eq(true)
+  end
+
+  it 'use variables to test your code' do
+    actual_value = 1 + 1
+    expected_value = 2
+
+    expect(expected_value).to eq(actual_value)
+  end
+
+  it 'test that the values are different' do
+    actual_value = 1 + 1
+    expected_value = 3
+
+    expect(expected_value).not_to eq(actual_value)
+  end
 end
